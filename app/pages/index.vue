69221ca67030b5b1f5b070a441607af4c682a226
yoshikouki <yoshikouki@gmail.com>
2020-07-27 23:20:48 +0900

    ホーム画面のレイアウトを仮作成
    - タブの切り替えによって表示するリストを切り替える機能を未実装

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,95 +1,43 @@
 <template>
-  <v-layout column justify-center align-center>
-    <v-flex xs12 sm8 md6>
-      <div class="text-center">
-        <logo />
-        <vuetify-logo />
-      </div>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>
-            Vuetify is a progressive Material Design component framework for
-            Vue.js. It was designed to empower developers to create amazing
-            applications.
-          </p>
-          <p>
-            For more information on Vuetify, check out the
-            <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation </a
-            >.
-          </p>
-          <p>
-            If you have questions, please join the official
-            <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord </a
-            >.
-          </p>
-          <p>
-            Find a bug? Report it on the github
-            <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board </a
-            >.
-          </p>
-          <p>
-            Thank you for developing with Vuetify and I look forward to bringing
-            more exciting features in the future.
-          </p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3" />
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br />
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn color="primary" nuxt to="/inspire">
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-flex>
-  </v-layout>
+  <v-tabs-items v-model="model">
+    <v-tab-item
+      v-for="(item, i) in items"
+      :key="i"
+      :to="item.to"
+      v-text="item.title"
+    >
+      <v-list nav="nav">
+        <v-list-item
+          v-for="(subject, index) in subjects"
+          :key="index"
+          :to="subject.id"
+          router
+          exact
+        >
+          <v-list-item-content>
+            <v-list-item-title v-text="subject.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-tab-item>
+  </v-tabs-items>
 </template>
 
 <script>
-import Logo from "~/components/Logo.vue"
-import VuetifyLogo from "~/components/VuetifyLogo.vue"
-
 export default {
-  components: {
-    Logo,
-    VuetifyLogo,
+  data() {
+    return {
+      subjects: [
+        {
+          id: "test_id",
+          title: "test_title",
+        },
+        {
+          id: "test_2",
+          title: "test_2",
+        },
+      ],
+    }
   },
 }
 </script>
