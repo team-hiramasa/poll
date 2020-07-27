74aed419b0fbdc308e11849d6f6777fb1648cdbb
Yuki Sugihara <ken.kiva@gmail.com>
2020-07-27 20:42:52 +0900

    ファイル作成

---
+++ app/pages/subjects/edit/_id.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-layout>
+    <v-flex class="text-center">
+      <img src="/v.png" alt="Vuetify.js" class="mb-5" />
+      <blockquote class="blockquote">
+        <footer>
+          <small>
+            <em>This ID is "{{ $route.params.id }}"</em>
+          </small>
+        </footer>
+      </blockquote>
+    </v-flex>
+  </v-layout>
+</template>
