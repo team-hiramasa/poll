6833d1099f325198219d2b69fb80f4d8699f6553
yoshikouki <yoshikouki@gmail.com>
2020-08-17 12:02:43 +0900

    ヘッダー部分をテンプレートレイアウトからindex.vueへ移動

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,22 +1,5 @@
 <template>
-  <v-app dark>
-    <v-app-bar fixed app>
-      <v-tabs
-        v-model="tab"
-        class="elevation-2"
-        dark
-        centered="centered"
-        fixed-tabs
-      >
-        <v-tabs-slider />
-        <v-tab
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          v-text="item.title"
-        />
-      </v-tabs>
-    </v-app-bar>
+  <v-app>
     <v-main>
       <v-container>
         <nuxt />
@@ -32,16 +15,6 @@
 export default {
   data() {
     return {
-      items: [
-        {
-          title: "回答した質問",
-          to: "/#voted",
-        },
-        {
-          title: "作成した質問",
-          to: "/#created",
-        },
-      ],
       copyright: "created by Team Hiramasa",
     }
   },
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,32 +1,61 @@
 <template>
-  <v-tabs-items v-model="model">
-    <v-tab-item
-      v-for="(item, i) in items"
-      :key="i"
-      :to="item.to"
-      v-text="item.title"
-    >
-      <v-list nav="nav">
-        <v-list-item
-          v-for="(subject, index) in subjects"
-          :key="index"
-          :to="subject.id"
-          router
-          exact
-        >
-          <v-list-item-content>
-            <v-list-item-title v-text="subject.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-tab-item>
-  </v-tabs-items>
+  <v-app>
+    <v-app-bar fixed app>
+      <v-tabs
+        v-model="tab"
+        class="elevation-2"
+        dark
+        centered="centered"
+        fixed-tabs
+      >
+        <v-tabs-slider />
+        <v-tab
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          v-text="item.title"
+        />
+      </v-tabs>
+    </v-app-bar>
+    <v-tabs-items v-model="model">
+      <v-tab-item
+        v-for="(item, i) in items"
+        :key="i"
+        :to="item.to"
+        v-text="item.title"
+      >
+        <v-list nav="nav">
+          <v-list-item
+            v-for="(subject, index) in subjects"
+            :key="index"
+            :to="subject.id"
+            router
+            exact
+          >
+            <v-list-item-content>
+              <v-list-item-title v-text="subject.title" />
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-tab-item>
+    </v-tabs-items>
+  </v-app>
 </template>
 
 <script>
 export default {
   data() {
     return {
+      items: [
+        {
+          title: "回答した質問",
+          to: "/#voted",
+        },
+        {
+          title: "作成した質問",
+          to: "/#created",
+        },
+      ],
       subjects: [
         {
           id: "test_id",
