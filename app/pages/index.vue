e86b2c65d90eff6ad36df0358e68559dbb0505ed
yoshikouki <yoshikouki@gmail.com>
2020-09-07 21:28:29 +0900

    DBに登録されている質問一覧の表示機能を実装

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -17,41 +17,32 @@
         :key="i"
         :to="item.to"
         v-text="item.title"
-      >
-        <v-list nav="nav">
-          <!-- <v-list-item
-            v-for="(subject, index) in subjects"
-            :key="index"
-            :to="subject.title"
-            router
-            exact
-          >
-            <v-list-item-content>
-              <v-list-item-title v-text="subject.title" />
-            </v-list-item-content>
-          </v-list-item> -->
-        </v-list>
-      </v-tab-item>
+      />
     </v-tabs-items>
+    <v-list>
+      <template v-for="(subject, index) in subjects">
+        <v-list-item :key="index" :to="subject.id">
+          <v-list-item-content>
+            <v-list-item-title>{{ subject.title }} </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </template>
+    </v-list>
+    <p>これはpタグ：{{ subjects[0].title }}</p>
   </v-app>
 </template>
 
 <script>
 import firebase from "@/plugins/firebase"
 const db = firebase.firestore()
-const subjectsRef = db.collection("subjects").get()
-subjectsRef
-  .then((res) => {
-    const subjects = res
-    subjects.forEach((doc) => {
-      console.log(doc.id, "=>", doc.data())
-    })
-  })
-  .catch((err) => {
-    console.log("Error getting documents", err)
-  })
 
 export default {
+  async asyncData() {
+    // const subjects = params.id
+    return {
+      subjects: await getSubjects(),
+    }
+  },
   data() {
     return {
       items: [
@@ -66,5 +57,39 @@ export default {
       ],
     }
   },
+}
+
+async function getSubjects() {
+  return await db
+    .collection("subjects")
+    .get()
+    .then((res) => {
+      console.log(
+        "startGetSubjects ==============================================================="
+      )
+      res.forEach((doc) => {
+        console.log(doc.id, "=>", doc.data())
+      })
+      console.log(
+        "end ============================================================================"
+      )
+      const subjects = []
+      res.forEach((doc) => {
+        const params = doc.data()
+        subjects.push({
+          id: doc.id,
+          title: params.title,
+          authId: params.authId,
+          isPublic: params.isPublic,
+          isCloseVoted: params.isCloseVoted,
+          visibleOrder: params.visibleOrder,
+          createdAt: params.createdAt,
+        })
+      })
+      return subjects
+    })
+    .catch((err) => {
+      console.log("Error getting documents", err)
+    })
 }
 </script>
