aa0cc0ed6c8186c60d9ffdc65aebb3983b228f4e
twinfonica <twinfonica@gmail.com>
2020-11-16 22:42:14 +0900

    投票結果を得票数の降順にソートした

--- app/pages/_id/index.vue
+++ app/pages/_id/index.vue
@@ -25,14 +25,10 @@
     </div>
     <div v-else class="result">
       投票結果<br /><br />
-      <ul v-if="isCloseVoted === true">
+      <ul>
         <li v-for="option in options" :key="option.id" class="list-option">
-          {{ option.title }}（{{ voteScores[option.id] || 0 }}）
-        </li>
-      </ul>
-      <ul v-else>
-        <li>
-          test
+          {{ option.title }}
+          <span v-if="isCloseVoted === true">（{{ option.score }}）</span>
         </li>
       </ul>
       <!-- TODO: 下記リンク先を画面遷移なしで出来ればそうする -->
@@ -58,7 +54,7 @@ export default {
       isCloseVoted: docData.isCloseVoted,
       isMine: docData.createdMe,
       isPublic: docData.isPublic,
-      options: questionData.options,
+      options: questionData.options, // 選択肢の配列. 得票数も格納する
       subjectId: currentSubjectId,
       title: docData.title,
       visibleOrder: docData.visibleOrder,
@@ -67,7 +63,6 @@ export default {
   },
   data: () => ({
     checkedOption: "",
-    voteScores: {},
     showResult: false,
     voteComment: "",
   }),
@@ -102,33 +97,44 @@ export default {
         this.afterVote()
       }
     },
+
     // 投票後の遷移
     afterVote() {
       if (this.isMine || this.isPublic) {
-        // 自分の質問 or すぐ結果を表示する設定なら、投票結果を取得＆表示する
-        this.displayResult()
+        // 自分の質問 or すぐ結果を表示する設定なら, 投票結果を取得＆表示する
+        this.showResult = true
+        this.getResult()
       } else {
         // トップ（投票一覧）に戻る
         location.href = "/"
       }
     },
-    // 投票結果を表示
-    displayResult() {
-      const objectVotes = {}
+
+    // 投票結果を選択肢配列に反映する
+    // TODO: isCloseVoted, visibleOrder の値に応じた結果にする
+    getResult() {
       db.collection("votes")
         .where("subjectId", "==", this.subjectId)
         .get()
         .then((docs) => {
+          // 選択肢IDごとに得票数を格納する一時オブジェクトを作成
+          const scores = {}
           docs.forEach((doc) => {
             const optionId = doc.data().optionId
-            if (objectVotes[optionId]) {
-              objectVotes[optionId]++
+            if (!scores[optionId]) {
+              scores[optionId] = 1
             } else {
-              objectVotes[optionId] = 1
+              scores[optionId]++
             }
           })
-          this.voteScores = objectVotes
-          this.showResult = true
+          // 得票数を選択肢配列に転記
+          this.options.forEach((option) => {
+            option.score = scores[option.id] || 0
+          })
+          // 選択肢配列を得票数の降順にソート
+          this.options.sort((a, b) => {
+            return a.score < b.score
+          })
         })
         .catch((error) => {
           console.log("[ERROR] in afterVote: ", error)
@@ -166,7 +172,7 @@ async function getQuestionData(authId, subjectId) {
   const optionsAry = []
   querySnapshot.forEach((doc) => {
     const data = doc.data()
-    optionsAry.push({ id: doc.id, title: data.title })
+    optionsAry.push({ id: doc.id, title: data.title, score: 0 })
   })
 
   // 質問に回答済みか否かを取得
