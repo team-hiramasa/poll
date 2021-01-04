997a02c09a1c84e9aec46cc03ce370d9b797e9fe
yoshikouki <yoshikouki@gmail.com>
2021-01-04 23:01:15 +0900

    メソッドをリファクタリングした

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -39,7 +39,7 @@ const tab = {
 export default {
   async asyncData() {
     return {
-      subjects: await getVotedSubjects(),
+      subjects: await fetchSubjectsList(tab.voted),
     }
   },
   data() {
@@ -47,82 +47,50 @@ export default {
   },
   methods: {
     async fetchVoted() {
-      this.subjects = await getVotedSubjects()
+      this.subjects = await fetchSubjectsList(tab.voted)
     },
     async fetchCreated() {
-      this.subjects = await getCreatedSubjects()
+      this.subjects = await fetchSubjectsList(tab.created)
     },
   },
 }
 
-// 自分が投稿した質問を配列で返す
-async function getVotedSubjects() {
-  let subjectList = null
-  await firebase.auth().onAuthStateChanged((user) => {
-    subjectList = fetchSubjectsList(user, tab.voted)
-  })
-  return subjectList
-}
-
-// 自分が作成した質問を配列で返す
-async function getCreatedSubjects() {
-  let subjectList = null
-  await firebase.auth().onAuthStateChanged((user) => {
-    subjectList = fetchSubjectsList(user, tab.created)
-  })
-  return subjectList
-}
-
 // firebase から質問一覧を取得して配列に変換して返す
-async function fetchSubjectsList(user, type) {
-  return await db
-    .collection(type)
-    .where("authId", "==", user.uid)
-    .get()
-    .then((snapshot) => {
-      return convertToSubjectsList(snapshot, type)
-    })
-    .catch((err) => {
-      console.log("Error getting documents", err)
-    })
-}
-
-// タイプによって適切な変換する
-function convertToSubjectsList(snapshot, type) {
-  if (snapshot.empty) {
-    console.log("No matching documents.")
-    return []
-  } else if (type === tab.voted) {
-    return subjectsListFromVotesCollection(snapshot)
-  } else if (type === tab.created) {
-    return subjectsListFromSubjectsCollection(snapshot)
-  }
-}
-
-// votes コレクションから取得したレコードを質問配列へと変換する
-function subjectsListFromVotesCollection(snapshot) {
-  const subjects = []
-  snapshot.forEach((doc) => {
-    const params = doc.data()
-    subjects.push({
-      id: params.subjectId,
-      title: params.questionTitle,
-      ...params,
-    })
+// 自分が投稿した質問 : 引数 votes
+// 自分が作成した質問 : 引数 subjects
+async function fetchSubjectsList(collectionName) {
+  const subjectList = []
+  await firebase.auth().onAuthStateChanged((user) => {
+    db.collection(collectionName)
+      .where("authId", "==", user.uid)
+      .get()
+      .then((snapshot) => {
+        if (snapshot.empty) {
+          console.log("No matching documents.")
+        } else {
+          snapshot.forEach((doc) => {
+            const params = doc.data()
+            if (collectionName === tab.voted) {
+              // 作成した質問
+              subjectList.push({
+                id: params.subjectId,
+                title: params.questionTitle,
+                ...params,
+              })
+            } else if (collectionName === tab.created) {
+              // 回答した質問
+              subjectList.push({
+                id: doc.id,
+                ...params,
+              })
+            }
+          })
+        }
+      })
+      .catch((err) => {
+        console.log("Error getting documents", err)
+      })
   })
-  return subjects
-}
-
-// firestore から取得したレコードを質問配列へと変換する
-function subjectsListFromSubjectsCollection(snapshot) {
-  const subjects = []
-  snapshot.forEach((doc) => {
-    const params = doc.data()
-    subjects.push({
-      id: doc.id,
-      ...params,
-    })
-  })
-  return subjects
+  return subjectList
 }
 </script>
