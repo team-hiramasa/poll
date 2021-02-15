87fa60708b4d6655826974edf3542bc3b32d48a8
Task Nagashige <task.19.xx@gmail.com>
2021-02-15 20:33:19 +0900

    [Fix] ビルドの設定をSPAになっていなかったのを修正

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,7 +12,7 @@ const config: Configuration = {
    ** Nuxt target
    ** See https://nuxtjs.org/api/configuration-target
    */
-  target: "server",
+  target: "static",
   /*
    ** Headers of the page
    ** See https://nuxtjs.org/api/configuration-head
